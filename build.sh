c821ab8afc0076c013f8c883bbb37006cbfc74ba
Aaron Alquist <aaalquis@akamai.com>
2019-08-21 23:52:32 -0400

    push the name without the hash

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 cd cli-query
 printf "\n" >> build-3.7.sh
-echo 'docker push $DOCKERNAME:$commitId' >> build-3.7.sh
+echo 'docker push $DOCKERNAME' >> build-3.7.sh
 cat build-3.7.sh
 bash build-3.7.sh
 cd -
